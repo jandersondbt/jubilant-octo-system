de8d51e279046bdeb35d21e5f2bd0eb99d94b2b5
bell <kazque@proton.me>
2025-12-05 14:40:46 -0300

    Another one to be sure

--- chapter_09/restaurant.py
+++ chapter_09/restaurant.py
@@ -2,4 +2,6 @@ class Restaurant:
     def __init__(self, restaurant_name, cuisine_type):
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
-        pass
+    #
+    def describe_restaurant(self):
+        print(f'{self.restaurant_name} is the best restaurant in the world!')
